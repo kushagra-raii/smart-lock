1d52a5aaf4d0aa803728efef82521a409b2d8460
Kushagra Rai <kushagrarai83@gmail.com>
2024-11-27 13:44:13 +0530

    Update index.js
    dynamic port

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors"
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 // Middleware
